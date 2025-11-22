34a89b32395c628e2394662869e10b03ce59175c
montserrat <quyennguyen083004@gmail.com>
2025-11-22 22:55:44 +0700

    Update app.js

--- app.js
+++ app.js
@@ -54,13 +54,7 @@ app.use(function(req, res, next) {
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = err.stack ? err.stack : err;
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    next(createError(404));
 });
 
 module.exports = app;
