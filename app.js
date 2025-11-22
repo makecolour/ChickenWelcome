e978955280cb9ffa6cd6b504895462030e9f4a93
montserrat <quyennguyen083004@gmail.com>
2025-11-22 22:57:24 +0700

    Improve error handling and set NODE_ENV to development
    Updated the error handler in app.js to render the error page with appropriate status and error details. Changed NODE_ENV in bin/www from 'production' to 'development' for development environment configuration.

--- app.js
+++ app.js
@@ -54,7 +54,12 @@ app.use(function(req, res, next) {
 
 // error handler
 app.use(function(err, req, res, next) {
-    next(createError(404));
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 module.exports = app;
